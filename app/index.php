bc2ba616a867471178c11f93809d9d3a44da9cf1
Thulio Philipe <thulioph@gmail.com>
2014-10-20 17:45:19 -0300

    Remove include do header e insere direto na index

--- app/index.php
+++ app/index.php
@@ -1,4 +1,37 @@
-<?php include('inc/header.php') ?>
+<?php include('Constants.php'); ?>
+
+<!doctype html>
+<html xmlns:og="http://ogp.me/ns#" lang="pt-br">
+<head>
+	<meta charset="utf-8" />
+
+	<title><?php echo _TITLE ?></title>
+
+  <!-- Facebook  -->
+  <meta property="og:type" content="business.business">
+  <meta property="og:url" content="http://pedalai.com.br">
+  <meta property="og:title" content="Pedalaí">
+  <meta property="og:image" content="http://pedalai.com.br/img/pedalai-facebook.png">
+  <meta property="og:description" content="WebApp desenvolvido para incentivar os ciclistas a pedalarem em grupo ou individual.">
+
+  <!-- Twitter -->
+  <meta name="twitter:card" value="summary">
+  <meta name="twitter:creator" value="@pedalaiapp">
+  <meta name="twitter:site" value="@pedalaiapp">
+
+  <!-- Apple Touch Icon -->
+  <link rel="apple-touch-icon" href="touch-icon@2x.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="touch-icon-ipad.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="touch-icon@2x.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad@2x.png">
+
+  <!-- Humans -->
+  <link type="text/plain" rel="author" href="http://pedalai.com.br/humans.txt" />
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="dist/css/styles.combined.min.css">
+
+</head>
 
 <body data-ng-app="APP">
 	<header>
@@ -14,14 +47,11 @@
 	<div data-ng-view class="content"></div>
 	<!-- view - end -->
 
-	<!-- libs - start -->
+	<!-- Scripts -->
 	<script src="libs/angular/angular.min.js"></script>
 	<script src="libs/angular-route/angular-route.min.js"></script>
 	<script src='http://maps.googleapis.com/maps/api/js?sensor=false'></script>
-	<!-- libs - end -->
 
-	<!-- development - start -->
 	<script src="dist/js/scripts.combined.min.js"></script>
-	<!-- development - end -->
 </body>
 </html>
